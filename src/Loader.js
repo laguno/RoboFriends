1a5a29eb26041c90050cba429c272bc35de05d98
Alessandro Messori <alessandro.messori@47deck.it>
2018-07-20 15:00:11 +0200

    Add Loader

---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loader = () => {
+	return(
+		<div>>
+			<div className="loader"></div>
+			<h1 className="tc f1 loader--text">loading robots...</h1>
+		</div>
+	)
+} 
+
+export default Loader;
